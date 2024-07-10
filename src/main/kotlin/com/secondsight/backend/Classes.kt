690a11cc8d240bcd89ca523d9d9b84a0c76206ef
Arnab Ghosh <arnabcare21@gmail.com>
2024-07-10 17:47:09 -0600

    Initial setup

--- src/main/kotlin/com/secondsight/backend/Classes.kt
+++ src/main/kotlin/com/secondsight/backend/Classes.kt
@@ -1,2 +1,23 @@
 package com.secondsight.backend
 
+import java.util.*
+
+/**
+ * User class
+ */
+class User (
+    val id: Int,
+    val name: String,
+    val email: String,
+    val notes: List<Note>,
+    
+) {
+
+}
+
+
+/**
+ * TODO: implement
+ */
+
+class Note (val id: Int, val owner: User, val createdAt: Date, val updatedAt: Date, )
